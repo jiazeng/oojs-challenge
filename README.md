f63a198b512e15aadc376c5571634ace13fb169d
jiazeng <sophbj93@uw.edu>
2014-12-07 20:57:54 -0800

    Done

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ oojs-challenge
 ==============
 
 File stubs and assets for the OOJS challenge
+
+By completing this challenge you will practice and demonstrate mastery of the following skills:
+
+Creating an object with both data and methods that operate on that data
+Creating a simple hierarchy of objects that inherit from one another using both a classical and a prototypal style
+Creating mix-in functions that can add new functionality to existing objects without requiring inheritance
--- js/shapes.js
+++ js/shapes.js
@@ -13,3 +13,61 @@
     or registerClassicalShape() to register your new shapes with the application. See the
     app.js file for info on these functions.
  */
+
+function Rectangle (left, top, width, height, stylesMap) {
+    this.left = left;
+    this.top = top;
+    this.width = width;
+    this. height = height;
+    this.stylesMap = stylesMap;
+
+    this.renderShape = function(canvasCtx) {
+        return canvasCtx.fillRect(this.left, this.top, this.width, this.height, this.stylesMap);
+    };
+}
+Rectangle.prototype = new Shape();
+registerClassicalShape('Rectangle', Rectangle);
+
+function Circle (left, top, width, height, stylesMap) {
+    this.left = left;
+    this.top = top;
+    this.width = width;
+    this.height = height;
+    this.stylesMap = stylesMap;
+
+    this.renderShape = function(canvasCtx) {
+        canvasCtx.beginPath();
+        canvasCtx.arc(this.left, this.top, this.height, 0, 2*Math.PI, this.stylesMap);
+        return canvasCtx.fill();
+    };
+}
+Circle.prototype = new Shape();
+registerClassicalShape('Circle', Circle);
+
+function Text (left, top, width, height, stylesMap) {
+    this.left = left;
+    this.top = top;
+    this.width = width;
+    this.height = height;
+    this.stylesMap = stylesMap;
+
+    this.renderShape = function(canvasCtx) {
+        var c = document.getElementById('shapes-canvas');
+        canvasCtx.font="30px Georgia";
+        canvasCtx.fillText("Big smile!",320,220);
+
+        canvasCtx.font="30px Verdana";
+        // Create gradient
+        var gradient=canvasCtx.createLinearGradient(0,0,c.width,0);
+        gradient.addColorStop("0","magenta");
+        gradient.addColorStop("0.5","blue");
+        gradient.addColorStop("1.0","red");
+        // Fill with gradient
+        canvasCtx.fillStyle=gradient;
+        canvasCtx.fillText("Hello World",300,280)
+    };
+}
+
+Text.prototype = new Shape();
+registerClassicalShape('Text', Text);
+
